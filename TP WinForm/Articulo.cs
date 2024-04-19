3538af76c4d7bd92692990b79db388d0d6f31319
unknown <francocataldo7@gmail.com>
2024-04-19 07:29:39 -0300

    solo clases

---
+++ TP WinForm/Articulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm
+{
+    internal class Articulo
+    {
+        public string CodArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca MarcaArt { get; set; }
+        public Categoria CategoriaArt { get; set; }
+        public Imagen ImgArt { get; set; }
+        public float Precio { get; set; }
+    }
+}
