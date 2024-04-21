9c21e3ac74e3d1461119b0acaea6a0739ef488be
unknown <francocataldo7@gmail.com>
2024-04-20 22:14:55 -0300

    eventos boton

--- TP WinForm/Form1.Designer.cs
+++ TP WinForm/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TP_WinForm
 {
-    partial class Form1
+    partial class frmCatalogo
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,33 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Btn_saludar = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.tbBuscador = new System.Windows.Forms.TextBox();
+            this.lblFiltrar = new System.Windows.Forms.Label();
+            this.btnDetalle = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.gvCatalogo = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCatalogo)).BeginInit();
             this.SuspendLayout();
             // 
-            // Btn_saludar
+            // tbBuscador
             // 
-            this.Btn_saludar.Location = new System.Drawing.Point(355, 98);
-            this.Btn_saludar.Name = "Btn_saludar";
-            this.Btn_saludar.Size = new System.Drawing.Size(289, 43);
-            this.Btn_saludar.TabIndex = 0;
-            this.Btn_saludar.Text = "button1";
-            this.Btn_saludar.UseVisualStyleBackColor = true;
+            this.tbBuscador.Location = new System.Drawing.Point(74, 12);
+            this.tbBuscador.Name = "tbBuscador";
+            this.tbBuscador.Size = new System.Drawing.Size(466, 20);
+            this.tbBuscador.TabIndex = 0;
             // 
-            // Form1
+            // lblFiltrar
+            // 
+            this.lblFiltrar.AutoSize = true;
+            this.lblFiltrar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFiltrar.Location = new System.Drawing.Point(22, 15);
+            this.lblFiltrar.Name = "lblFiltrar";
+            this.lblFiltrar.Size = new System.Drawing.Size(35, 15);
+            this.lblFiltrar.TabIndex = 0;
+            this.lblFiltrar.Text = "FIltrar";
+            // 
+            // btnDetalle
+            // 
+            this.btnDetalle.Location = new System.Drawing.Point(22, 399);
+            this.btnDetalle.Name = "btnDetalle";
+            this.btnDetalle.Size = new System.Drawing.Size(75, 23);
+            this.btnDetalle.TabIndex = 2;
+            this.btnDetalle.Text = "Detalle";
+            this.btnDetalle.UseVisualStyleBackColor = true;
+            this.btnDetalle.Click += new System.EventHandler(this.btnDetalle_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(159, 399);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(314, 399);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 4;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(465, 399);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // gvCatalogo
+            // 
+            this.gvCatalogo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvCatalogo.Location = new System.Drawing.Point(22, 38);
+            this.gvCatalogo.Name = "gvCatalogo";
+            this.gvCatalogo.Size = new System.Drawing.Size(518, 355);
+            this.gvCatalogo.TabIndex = 6;
+            // 
+            // frmCatalogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Btn_saludar);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.gvCatalogo);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnDetalle);
+            this.Controls.Add(this.lblFiltrar);
+            this.Controls.Add(this.tbBuscador);
+            this.Name = "frmCatalogo";
+            this.Text = "Catalogo";
+            ((System.ComponentModel.ISupportInitialize)(this.gvCatalogo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button Btn_saludar;
+        private System.Windows.Forms.TextBox tbBuscador;
+        private System.Windows.Forms.Label lblFiltrar;
+        private System.Windows.Forms.Button btnDetalle;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.DataGridView gvCatalogo;
     }
 }
 
--- TP WinForm/Form1.cs
+++ TP WinForm/Form1.cs
@@ -10,11 +10,32 @@ using System.Windows.Forms;
 
 namespace TP_WinForm
 {
-    public partial class Form1 : Form
+    public partial class frmCatalogo : Form
     {
-        public Form1()
+        public frmCatalogo()
         {
             InitializeComponent();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDetalle_Click(object sender, EventArgs e)
+        {
+            Detalle ventanaDetalle = new Detalle();
+            ventanaDetalle.ShowDialog();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
